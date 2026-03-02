27a3b28009fe77334ec1e17ada73301b64b6281f
Ivan Maksymenko <ivan.maksymenko.01@gmail.com>
2026-03-02 13:55:03 +0100

    logo path change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const { lang, setLang, urlPrefix, t } = useLanguage();
-  const logoSrc = "src/assets/logo/logo.png";
+  const logoSrc = `${import.meta.env.BASE_URL}/willbebar/@main/logo/logo.png`;
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
